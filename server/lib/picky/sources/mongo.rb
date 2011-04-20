6e83da524fb94a1947b920e75ade4c10e75f78b1
ClintKrollwood <simon.krollpfeifer@peritor.com>
2011-04-20 11:57:10 +0200

    mongo.db source with messed up tests added

--- server/lib/picky/sources/mongo.rb
+++ server/lib/picky/sources/mongo.rb
@@ -1,21 +1,24 @@
 module Sources
 
-  # Raised when a Couch source is instantiated without a file.
-  #
-  # Example:
-  #   Sources::Couch.new(:column1, :column2) # without file option
-  #
+  # Raised when a Mongo source is instantiated without a valid uri.
+	#
+	# Important!
+  # You have to start your mongodb with --rest in order to use
+  # the rest / http interface 
+	#
   class NoMongoDBGiven < StandardError; end
 
-  # A Couch database source.
+  # A Mongo database source.
   #
   # Options:
-  # * url
+  # * url, db
+  # Example:
+  #   Sources::Mongo.new(:collection1, :collection2, :url => 'localhost:28017', :db => 'testdatabase')
+	# Be sure to escape the URL properly, e.g. # => %23 in the databasename if needed
+	#
   # and all the options of a <tt>RestClient::Resource</tt>.
   # See http://github.com/archiloque/rest-client.
   #
-  # Examples:
-  #  http://123.123.123.123:3124/database/collection/query
 
   class Mongo < Base
 		@@id_key = '_id'
@@ -30,12 +33,7 @@ module Sources
 		
 		  @db = RestClient::Resource.new options.delete(:url), options
 			@database = options.delete(:db)
-	
-		  @key_format  = key_format && key_format.to_sym || :to_sym
-		end
-
-		def initialize
-			@key_format = :to_sym
+		  @key_format  = options.try(:[], :key_format).try(:to_sym) || :to_sym
 		end
 
     # Tries to require the rest_client gem.
@@ -47,7 +45,12 @@ module Sources
       exit 1
     end
 
-
+		# Fetches the data, @limit=0 will return all records 
+		#
+		# Limit is set to 0 by default - all collection entries will be send
+		# If want to limit the results, set to to any other number, e.g. limit=15
+		# to return only 15 entries
+		#
 		def harvest category
 			collection = category.from.to_s || category.index_name.to_s
 			resp = @db["/#{@database}/#{category.index_name.to_s}/?@limit=0"].get
